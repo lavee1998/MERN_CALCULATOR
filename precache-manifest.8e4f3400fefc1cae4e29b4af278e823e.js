a9616558c11b378b176e59562f2ce75543ae7f8b
lavee1998 <bk29f1@inf.elte.hu>
2020-09-16 23:39:38 +0200

    Updates

---
+++ precache-manifest.8e4f3400fefc1cae4e29b4af278e823e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed1acf0c0c8e11223978d9d91e65702",
+    "url": "/MERN_CALCULATOR/index.html"
+  },
+  {
+    "revision": "09710776fa4e538e8615",
+    "url": "/MERN_CALCULATOR/static/css/main.69dbf060.chunk.css"
+  },
+  {
+    "revision": "e84f34e993fa0b144cf0",
+    "url": "/MERN_CALCULATOR/static/js/2.7359c3af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MERN_CALCULATOR/static/js/2.7359c3af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09710776fa4e538e8615",
+    "url": "/MERN_CALCULATOR/static/js/main.e77302a5.chunk.js"
+  },
+  {
+    "revision": "5d9b51daa7abf731d1f9",
+    "url": "/MERN_CALCULATOR/static/js/runtime-main.75c10f58.js"
+  }
+]);
